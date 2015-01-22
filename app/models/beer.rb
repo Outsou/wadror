699326e17611d8391e7a54b16a97c6b3a74e2181
Otto Hantula <otto.hantula@hotmail.com>
2015-01-22 14:47:44 +0200

    vko2 t4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    sum = 0.0
+    ratings.each do |rating|
+      sum += rating.score
+    end
+    sum/ratings.count
+  end
 end
