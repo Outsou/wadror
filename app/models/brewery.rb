4256eccf60831f83dddac23ad8ae7bc0023b76f7
Otto Hantula <otto.hantula@hotmail.com>
2015-01-22 20:10:05 +0200

    vko2 t14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,8 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def average_rating
+    ratings.inject(0) { |sum, r| sum + r.score }.to_f / ratings.count
+  end
 end
