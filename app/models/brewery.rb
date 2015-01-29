a084bccbc28ba3febc3bd445cd70bba5791a0215
Otto Hantula <otto.hantula@hotmail.com>
2015-01-29 15:11:06 +0200

    vko3 t6

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,9 +3,15 @@ class Brewery < ActiveRecord::Base
 
   validates :name, presence: true
   validates :year, numericality: { greater_than_or_equal_to: 1042,
-                                    less_than_or_equal_to: 2015,
                                     only_integer: true }
+  validate :year_cannot_be_in_the_future
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def year_cannot_be_in_the_future
+    if year > Time.now.year
+      errors.add(:year, "can't be in the future")
+    end
+  end
 end
