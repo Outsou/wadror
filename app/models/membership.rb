df73e710501da39c8edefa914109a3c2591ee6f6
Otto Hantula <otto.hantula@hotmail.com>
2015-01-29 17:01:53 +0200

    vko3 t9

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,12 @@
 class Membership < ActiveRecord::Base
+  validate :user_already_member
+
   belongs_to :user
   belongs_to :beer_club
+
+  def user_already_member
+    if not Membership.find_by(user_id:user_id, beer_club_id:beer_club_id).nil?
+      errors.add(:base, "You are a member of this club already.")
+    end
+  end
 end
