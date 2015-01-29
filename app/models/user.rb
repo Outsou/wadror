a7d9b7122cfd3dd1ca655946c6707e689c4dc21f
Otto Hantula <otto.hantula@hotmail.com>
2015-01-29 18:36:07 +0200

    vko t12-13

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
   validates :password, length: { minimum: 4 }
   validates_format_of :password, :with => /(?=.*[A-Z])(?=.*[0-9])/
 
-  has_many :ratings
-  has_many :memberships
+  has_many :ratings, dependent: :destroy
+  has_many :memberships, dependent: :destroy
   has_many :beers, through: :ratings
   has_many :beer_clubs, through: :memberships
 
