6a9ab017c2fa7c6dc67db6e51cd05f32d5121e2b
Otto Hantula <otto.hantula@hotmail.com>
2015-02-18 13:59:05 +0200

    fixed tests

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -1,21 +1,15 @@
 require 'rails_helper'
 
 describe "Ratings page" do
-  it "should not have any before been created" do
-    visit ratings_path
-    expect(page).to have_content 'List of ratings'
-    expect(page).to have_content 'Number of ratings: 0'
-  end
 
   describe "when ratings exist" do
-    it "lists the existing ratings and their total number" do
+    it "lists the existing ratings" do
       user = FactoryGirl.create(:user)
 
       ratings = create_ratings(user)
 
       visit ratings_path
-
-      expect(page).to have_content "Number of ratings: #{ratings.count}"
+      
       ratings.each do |rating|
         expect(page).to have_content rating.beer.name
       end
